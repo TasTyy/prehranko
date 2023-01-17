847555877928d41d75e6256c9827b7730e26b94c
TasTyy <maj.koren@gmail.com>
2023-01-17 02:14:45 +0100

    Home page.

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class home extends StatelessWidget {
+class home extends StatefulWidget {
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  DateTime _currentdate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Prehranko'),
+        title: Text(DateFormat('yMMMMd').format(_currentdate)),
       ),
     );
   }
